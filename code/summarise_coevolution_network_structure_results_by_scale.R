1fb081ca2f82603c5132c0a997a12d0ad2ed15f3
fp3draza <65344029+fp3draza@users.noreply.github.com>
2023-07-03 15:13:22 +0200

    Update summarise_coevolution_network_structure_results_by_scale.R

--- code/summarise_coevolution_network_structure_results_by_scale.R
+++ code/summarise_coevolution_network_structure_results_by_scale.R
@@ -11,6 +11,10 @@ complete_data_network_scale_summary <- complete_data %>%
                                                         group_by(network_name, network_type, anta_ratio, sequence) %>%
                                                         mutate(is_na = if_else(is.na(network_indirect_effects), 1, 0)) %>% 
                                                         summarise(time_to_equilibrium = mean(time_to_equilibrium, na.rm = TRUE),
+                                                                  size = mean(nsp, na.rm = TRUE),
+                                                                  modularity_z = mean(modularity_z, na.rm = TRUE),
+                                                                  nestedness_z = mean(nestedness_z, na.rm = TRUE),
+                                                                  connectance = mean(connectance, na.rm = TRUE),
                                                                   rate_of_adaptive_change = mean(rate_of_adaptive_change, na.rm = TRUE),
                                                                   network_indirect_effects = mean(network_indirect_effects, na.rm = TRUE),
                                                                   network_trait_change = mean(network_trait_change, na.rm = TRUE),
@@ -18,6 +22,7 @@ complete_data_network_scale_summary <- complete_data %>%
                                                                   network_matching_all_species = mean(network_matching_all_species_no_diagonal, na.rm = TRUE),
                                                                   network_matching_interacting_species = mean(network_matching_only_interactions, na.rm = TRUE),
                                                                   pca_observed_values = mean(pca_observed_values, na.rm = TRUE),
+                                                                  pca_zscore = mean(pca_zscore_values, na.rm = TRUE),
                                                                   pca_observed_values_updated = mean(pca_observed_values_updated, na.rm = TRUE),
                                                                   pca_size_connectance = mean(pca_size_connectance, na.rm = TRUE),
                                                                   pca_observed_values_by_network_type = mean(pca_observed_by_network_type, na.rm = TRUE),
@@ -50,7 +55,7 @@ complete_data_species_scale_summary_final <- complete_data_species_scale_summary
                   species_matching_only_interactions = mean(species_matching_only_interactions, na.rm = TRUE),
                   species_trait_change = mean(species_trait_change, na.rm = TRUE),
                   species_theta_matching = mean(species_theta_matching, na.rm = TRUE),
-                  pca_zscore_values = mean(pca_zscore_values, na.rm = TRUE),
+                  pca_zscore = mean(pca_zscore_values, na.rm = TRUE),
                   pca_size_connectance = mean(pca_size_connectance, na.rm = TRUE),
                   z_final_mean = mean(z_final, na.rm = TRUE),
                   z_final_sd = sd(z_final, na.rm = TRUE))
